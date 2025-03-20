b9bdfea7ba6bc9f4e984c19fecc62a36f75aad3c
hebulin <hebulin@qq.com>
2025-03-20 11:00:38 +0800

    优化了ali源deepseek的返回响应处理，配置改为动态配置，前提是需官网开通智能体应用，动态配置是获取appid

--- chatai-with-newbot/src/main/java/com/chatai/newbot/config/AIConfig.java
+++ chatai-with-newbot/src/main/java/com/chatai/newbot/config/AIConfig.java
@@ -1,24 +1,42 @@
 package com.chatai.newbot.config;
 
 import lombok.Data;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.chatai.newbot.constant.AiConstant.*;
+
+@Component
 @Data
-@Configuration
-@ConfigurationProperties(prefix = "ai")
 public class AIConfig {
+    @Value("${ai.ali-api-key}")
+    private String aliApiKey;
+
+    @Value("${ai.ali-api-url}")
+    private String aliApiUrl;
+
+    @Value("${ai.ali-app-id}")
+    private String aliAppId;
+
     /**
      * deepseek
      */
+    @Value("${ai.deepseek-key}")
     private String deepseekKey;
+    @Value("${ai.deepseek-url}")
     private String deepseekUrl;
+    @Value("${ai.deepseek-model}")
     private String deepseekModel;
-    /**
-     * 阿里
-     */
-    private String aliApiKey;
-    private String aliApiUrl;
-    private String aliAppId;
-    private String aliModel;
+
+    // 动态加载模型配置
+    public Map<String, String> getModelConfig(String model) {
+        Map<String, String> config = new HashMap<>();
+        config.put(API_KEY, System.getProperty(model + AI_API_KEY));
+        config.put(API_URL, System.getProperty(model + AI_API_URL));
+        config.put(APP_ID, System.getProperty(model + AI_APP_ID));
+        return config;
+    }
 }
---
+++ chatai-with-newbot/src/main/java/com/chatai/newbot/constant/AiConstant.java
@@ -0,0 +1,12 @@
+package com.chatai.newbot.constant;
+
+public class AiConstant {
+    // 模型配置 动态获取的key
+    public static final String API_KEY = "api-key";
+    public static final String API_URL = "api-url";
+    public static final String APP_ID = "app-id";
+    // 动态配置后缀
+    public static final String AI_API_KEY = ".ai.api-key";
+    public static final String AI_API_URL = ".ai.api-url";
+    public static final String AI_APP_ID = ".ai.app-id";
+}
