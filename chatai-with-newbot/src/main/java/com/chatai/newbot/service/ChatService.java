a658e3436c966bb1bafe403c60e889d50572bd36
hebulin <hebulin@qq.com>
2025-03-20 17:41:52 +0800

    统一tongyi接口 增加图片样式 动态property配置

--- chatai-with-newbot/src/main/java/com/chatai/newbot/service/ChatService.java
+++ chatai-with-newbot/src/main/java/com/chatai/newbot/service/ChatService.java
@@ -3,6 +3,6 @@ package com.chatai.newbot.service;
 import com.chatai.newbot.model.ChatRequest;
 import reactor.core.publisher.Flux;
 
-public interface TongYiService extends ChatService{
-    Flux<String> chatWithDeepThink(ChatRequest request);
+public interface ChatService {
+    Flux<String> chat(ChatRequest request);
 }
